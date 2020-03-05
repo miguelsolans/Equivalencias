9aab4d7a7e731adcef7471dd35c7ce1e26253600
Miguel Solans <dev.miguelsolans@gmail.com>
2020-03-05 13:59:01 +0000

    Password Encryption
    Password Encryption with EncryptJS

--- ServerAPI/app/routes/user.js
+++ ServerAPI/app/routes/user.js
@@ -7,16 +7,32 @@ const Users = require('../controllers/users');
 router.get('/:username', (req, res) => {
     console.log(`GET / ${req.params.username}`);
 
+    bcrypt.
     Users.searchUser(req.params.username)
         .then(data => res.jsonp(data))
         .catch(err => res.jsonp(err));
 });
 
 router.post('/', (req, res) => {
+    console.log("USER POST /");
 
-    Users.newUser(req.body)
-        .then(data => res.jsonp(data))
-        .catch(err => res.jsonp(err));
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        // TODO: Remove throw after tests
+        if(err) throw err;
+
+        console.log(`PASSWORD HASHED: ${hash}`);
+        // {username, password, email}
+        const newUser = {
+            username: req.body.username,
+            password: hash,
+            email: req.body.email
+        }
+
+        Users.newUser(newUser)
+            .then(data => res.jsonp(data))
+            .catch(err => res.jsonp(err));
+    });
+
 });
 
 
