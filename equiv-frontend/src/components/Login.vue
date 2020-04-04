6546375c39ccc50881e16d61b25434afd4de198d
Miguel Solans <dev.miguelsolans@gmail.com>
2020-04-04 17:43:08 +0100

    Frontend App Configuration

---
+++ equiv-frontend/src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-form ref="from" lazy-validation>
+        <v-container fluid>
+            <v-row>
+
+            </v-row>
+        </v-container>
+        <v-text-field label="Username" name="username"></v-text-field>
+
+        <v-text-field label="Password" name="password"
+                      :append-icon="value ? 'visibility' : 'visibility_off'"
+                      @click:append="() => (value = !value)"
+                      :type="value ? 'password' : 'text'"></v-text-field>
+    </v-form>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ equiv-frontend/src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-form ref="form">
+        <img src="../assets/Logo.png" style="width:auto; height: 50px;  align-content: center">
+        <v-text-field label="Username"
+                      name="username"
+                      id="username"
+                      v-model="username"
+                      type="text" required>
+
+        </v-text-field>
+        <v-text-field label="Password" name="password" id="password" v-model="password" type="password" required></v-text-field>
+        <v-btn class="mx-2" dark color="teal" @click="onLogin">Login</v-btn>
+    </v-form>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data() {
+            return {
+                username: '',
+                password: ''
+            }
+        },
+        methods: {
+            onLogin () {
+                fetch('http://localhost:3030/user/miguelsolans', {
+                    method: 'GET',
+                    mode: 'cors'
+                }).then(resp => resp.json())
+                    .then(data => console.log(data))
+                    .catch(err => console.log(err));
+                // alert(this.username);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
