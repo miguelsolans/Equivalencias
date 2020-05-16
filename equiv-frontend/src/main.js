5eac37dccbaf163663760091e7f031dd5152a1bd
Diogo Araújo <36507885+dichico@users.noreply.github.com>
2020-05-16 18:28:49 +0100

    pacote NPM para icones
    Co-Authored-By: Diogo Nogueira <diogoesnog@users.noreply.github.com>

--- equiv-frontend/src/main.js
+++ equiv-frontend/src/main.js
@@ -6,9 +6,16 @@ import vuetify from './plugins/vuetify';
 import VueCookies from 'vue-cookies';
 import VueSweetAlert2 from 'vue-sweetalert2';
 
+import library from '@fortawesome/fontawesome-svg-core';
+import faUserSecret from '@fortawesome/free-solid-svg-icons';
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+
 Vue.use(VueCookies);
 Vue.use(VueSweetAlert2);
 Vue.use(require('vue-moment'));
+
+library.add(faUserSecret);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
