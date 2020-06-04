23dc2c7cb1a341a1d822e3b911b987389d55db9b
Diogo Nogueira <34174814+diogoesnog@users.noreply.github.com>
2020-06-05 00:25:54 +0100

    Navbar quase feita
    Co-Authored-By: Diogo Araújo <dichico@users.noreply.github.com>

--- equiv-frontend/src/components/Navbar.vue
+++ equiv-frontend/src/components/Navbar.vue
@@ -1,64 +1,45 @@
 <template>
-  <v-container 
-    class="mb-7"
-  >
-    <v-app-bar 
-      app 
-      color="#333" 
-      dark 
-      dense 
-      short>
-      <v-btn icon large :to="{ name: 'dashboard' }">
-        <v-icon>mdi-home</v-icon>
-      </v-btn>
-      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
-        <span>MANTIS</span>
+  <v-container class="mb-7">
+    <v-app-bar app color="#313131" dark dense short elevate-on-scroll>
+      <v-spacer></v-spacer>
+      <v-toolbar-title style="width: 160px">
+        <img class="imgLogo pt-1" src="../assets/images/Logo Branco.png" />
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon large @click.stop="drawer = !drawer">
-        <v-avatar size="32px" icon>
+      <div class="text-center mx-2">
+        <v-btn rounded color="primary" dark>
+          <text-area class="text-capitalize mx-1 .d-sm-none .d-md-flex">
+            <strong>Página Inicial</strong>
+          </text-area>
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+      </div>
+      <div class="text-center mx-2">
+        <v-btn rounded color="primary" dark>
+          <text-area class="text-capitalize mx-1 .d-sm-none .d-md-flex">
+            <strong>Definições</strong>
+          </text-area>
           <v-icon>mdi-account-cog</v-icon>
-        </v-avatar>
-      </v-btn>
+        </v-btn>
+      </div>
+      <div class="text-center mx-2">
+        <v-btn rounded color="primary" dark>
+          <text-area class="text-capitalize mx-1 .d-sm-none .d-md-flex">
+            <strong>Sair</strong>
+          </text-area>
+          <v-icon>mdi-logout</v-icon>
+        </v-btn>
+      </div>
+      <v-spacer></v-spacer>
     </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" right app false>
-      <v-list-item-group>
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon>mdi-account-cog</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>Definições</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item @click="logoutAlert = true">
-          <v-list-item-icon>
-            <v-icon>mdi-logout</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>Terminar Sessão</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-navigation-drawer>
 
     <v-dialog v-model="logoutAlert" persistent max-width="350">
       <v-card>
         <v-card-title class="headline">Deseja terminar sessão?</v-card-title>
-        <v-card-text
-          >Todos as suas alterações não guardadas serão perdidas.</v-card-text
-        >
+        <v-card-text>Todos as suas alterações não guardadas serão perdidas.</v-card-text>
         <v-card-actions class="justify-center">
-          <v-btn color="green darken-1" text @click="logoutAlert = false"
-            >Voltar Atrás</v-btn
-          >
-          <v-btn color="red darken-1" text @click="handleLogout"
-            >Terminar Sessão</v-btn
-          >
+          <v-btn color="green darken-1" text @click="logoutAlert = false">Voltar Atrás</v-btn>
+          <v-btn color="red darken-1" text @click="handleLogout">Terminar Sessão</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -68,19 +49,24 @@
 <script>
 export default {
   props: {
-    source: String,
+    source: String
   },
   data: () => ({
-    drawer: false,
-    logoutAlert: false,
+    logoutAlert: false
   }),
   methods: {
     handleLogout() {
       this.$store
         .dispatch("auth/logout")
         .then(() => this.$router.push("/login"))
-        .catch((err) => console.log(err));
-    },
-  },
+        .catch(err => console.log(err));
+    }
+  }
 };
 </script>
+
+<style scoped>
+.imgLogo {
+  width: 100%;
+}
+</style>
