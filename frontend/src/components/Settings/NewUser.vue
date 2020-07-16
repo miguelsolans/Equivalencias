cf4671af5c7504e90091c75f385ab06e38c038f3
Miguel Solans <dev.miguelsolans@gmail.com>
2020-07-16 18:31:33 +0100

    Create new User Form

--- frontend/src/components/Settings/NewUser.vue
+++ frontend/src/components/Settings/NewUser.vue
@@ -1,12 +1,13 @@
 <template>
     <v-container>
         <v-form>
-            <v-text-field v-model="user.username" label="Nome do Utilizador"/>
-            <v-text-field v-model="user.email" label="Username"/>
-            <v-text-field v-model="user.email" label="E-mail"/>
-            <v-text-field v-model="user.email" label="E-mail"/>
+            <v-text-field v-model="user.username" label="Nome do Utilizador" type="text"/>
+            <v-text-field v-model="user.username" label="Username" type="text"/>
+            <v-text-field v-model="user.email" label="E-mail" type="email"/>
+            <v-text-field v-model="user.password" label="E-mail" type="password"/>
+            <v-checkbox v-model="user.admin" label="Conta de Administração"/>
 
-            <v-btn color="error" @click="handleSubmit">Alterar Password</v-btn>
+            <v-btn color="error" @click="handleSubmit">Criar Utilizador</v-btn>
         </v-form>
     </v-container>
 </template>
@@ -18,6 +19,11 @@
         data() {
             return {
                 user: new User(),
+            }
+        },
+        methods: {
+            handleSubmit(e) {
+                e.preventDefault();
             }
         }
     }
