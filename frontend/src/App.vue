1743d572026b16444d58dcd89cd87274de13fa7d
Diogo Nogueira <34174814+diogoesnog@users.noreply.github.com>
2020-07-16 21:47:06 +0100

    Footer retirado

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,12 +12,10 @@
 <script>
 
     import Navbar from "./components/Navbar";
-    import Footer from "./components/Footer";
 
     export default {
         components: {
-            Navbar,
-            Footer
+            Navbar
         },
         computed: {
             loggedOn() {
