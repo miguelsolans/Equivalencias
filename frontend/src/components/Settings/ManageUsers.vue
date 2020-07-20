627dd068708a9718e1f28b007ee85549491f79ba
Miguel Solans <dev.miguelsolans@gmail.com>
2020-07-20 18:51:35 +0100

    User Editing

--- frontend/src/components/Settings/ManageUsers.vue
+++ frontend/src/components/Settings/ManageUsers.vue
@@ -10,17 +10,39 @@
                 </v-icon>
             </template>
         </v-data-table>
-    </v-container>
 
+
+        <v-dialog v-model="editModal">
+            <v-card>
+                <v-card-title>Editar Utilizador: {{this.userEdit.username}}</v-card-title>
+
+                <v-card-text>
+                    <v-container>
+                        <v-form>
+                            <v-text-field v-model="userEdit.fullName" label="Nome Completo" type="text"/>
+                            <v-text-field v-model="userEdit.username" label="Username" type="text"/>
+                            <v-text-field v-model="userEdit.email" label="E-mail" type="email"/>
+                            <v-checkbox v-model="userEdit.admin" label="Administrador da Plataforma"></v-checkbox>
+
+                            <v-btn primary @click="handleSubmit">Guardar Alterações</v-btn>
+                        </v-form>
+                    </v-container>
+                </v-card-text>
+            </v-card>
+        </v-dialog>
+    </v-container>
 </template>
 
 <script>
     import UserService from '../../services/user.service';
+    import User from '../../models/user';
     export default {
         name: "ManageUsers",
         props: ["users"],
         data() {
             return {
+                userEdit: new User("", "", false, "", ""),
+                editModal: false,
                 headers: [
                     { text: "Nome", value: "fullName"},
                     { text: "Username", value: "username"},
@@ -33,8 +55,22 @@
         mounted() {
         },
         methods: {
+            handleSubmit(e) {
+                e.preventDefault();
+
+                UserService.editUser(this.userEdit)
+                    .then(response => {
+                        console.log(response.data);
+
+                    }).catch(err => console.log(err));
+            },
             editUser(item) {
                 console.log("Editing User");
+
+                this.editModal = true;
+
+                this.userEdit = new User(item.username, item.fullName, item.admin, item.email);
+
                 console.log(item.username);
             },
             deleteUser(item) {
